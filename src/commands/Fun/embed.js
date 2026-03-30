6c54238c22ecd63bc265779525ee03b8e0bebf92
banggive <68670750+banggive@users.noreply.github.com>
2026-03-30 14:58:09 +0700

    embed.js
    fixed embed

--- src/commands/Fun/embed.js
+++ src/commands/Fun/embed.js
@@ -1,26 +1,40 @@
+import { SlashCommandBuilder } from 'discord.js';
+import { successEmbed } from '../../utils/embeds.js';
+import { logger } from '../../utils/logger.js';
+import { handleInteractionError } from '../../utils/errorHandler.js';
+import { InteractionHelper } from '../../utils/interactionHelper.js';
+
 export default {
-  name: 'embed',
-  description: 'Kirim embed',
+  data: new SlashCommandBuilder()
+    .setName("embed")
+    .setDescription("Kirim embed custom")
+    .addStringOption(option =>
+      option.setName('text')
+        .setDescription('Isi pesan embed')
+        .setRequired(true)
+    ),
 
-  options: [
-    {
-      name: 'text',
-      description: 'Isi pesan',
-      type: 3,
-      required: true
+  category: 'Fun',
+
+  async execute(interaction, config, client) {
+    try {
+      const text = interaction.options.getString('text');
+
+      const embed = successEmbed("📢 Message", text);
+
+      await InteractionHelper.safeReply(interaction, {
+        embeds: [embed]
+      });
+
+      logger.debug(`Embed command used by ${interaction.user.id}`);
+
+    } catch (error) {
+      logger.error('Embed command error:', error);
+
+      await handleInteractionError(interaction, error, {
+        commandName: 'embed',
+        source: 'embed_command'
+      });
     }
-  ],
-
-  run: async ({ interaction }) => {
-    const text = interaction.options.getString('text');
-
-    await interaction.reply({
-      embeds: [
-        {
-          description: text,
-          color: 0x00AEFF
-        }
-      ]
-    });
-  }
+  },
 };
